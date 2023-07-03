ae0a3bab8bca5c5c4b8ec499a1d91db3f4334862
panggin123 <panggin159@naver.com>
2023-07-03 17:39:08 +0900

    asdf

--- src/main/java/com/mycompany/app/infra/code/CodeXdmController.java
+++ src/main/java/com/mycompany/app/infra/code/CodeXdmController.java
@@ -10,81 +10,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.mycompany.app.infra.codeGroup.CodeGroup;
 import com.mycompany.app.infra.codeGroup.CodeGroupVo;
 
-
-
 @Controller
 public class CodeXdmController {
 
 	@Autowired
 	CodeXdmServiceImpl service;
-	
-	@RequestMapping(value="/codeXdmList")
-	public String codeXdmList(CodeXdmVo vo ,Model model) {
-		
+
+	@RequestMapping(value = "/codeXdmList")
+	public String codeXdmList(CodeXdmVo vo, Model model) {
+
 		vo.setShKeyword(vo.getShKeyword() == null ? "회원" : vo.getShKeyword());
-		
+
 		List<CodeXdm> list = service.selectList(vo);
-		
-		model.addAttribute("list",list);
-		model.addAttribute("vo",vo);
-		
+
+		model.addAttribute("list", list);
+		model.addAttribute("vo", vo);
+
 		return "/xdm/infra/index/codeXdmList";
 	}
-	
-	@RequestMapping(value="/codeXdmForm")
+
+	@RequestMapping(value = "/codeXdmForm")
 	public String indexXdmViewForm(CodeXdmVo vo, Model model) {
-		
+
 		model.addAttribute("item", service.selectOne(vo));
-		
-		return"xdm/infra/index/codeXdmForm";
+
+		return "xdm/infra/index/codeXdmForm";
 	}
-	
-	@RequestMapping(value="/codeXdminsert")
+
+	@RequestMapping(value = "/codeXdminsert")
 	public String codeXdminsert(CodeXdmVo vo, Model model) {
-		
+
 		model.addAttribute("item", service.selectOne(vo));
-		
-		return"xdm/infra/index/codeXdminsert";
+
+		return "xdm/infra/index/codeXdminsert";
 	}
-	
-	
+
 	@RequestMapping("/codeXdmUpdate")
 	public String codeXdmUpdate(CodeXdm dto) {
-		
+
 		System.out.println("codeXdmUpdate");
-		
+
 		System.out.println("dto.getName(): " + dto.getName());
 		System.out.println("dto.getSeq(): " + dto.getSeq());
-		
+
 		service.update(dto);
-		
-		
+
 		return "redirect:/codeXdmList";
 	}
 
 	@RequestMapping("/codeXdmDelete")
 	public String codeXdmUele(CodeXdm dto) {
-	
-	
+
 		service.delete(dto);
-		
+
 		return "redirect:/codeXdmList";
-}
-	
-	
+	}
+
 	@RequestMapping("/codeXdminsertbtn")
 	public String codeXdminsertbtn(CodeXdm dto) {
-		
-	System.out.println("codeXdminsertbtn");
-		
+
+		System.out.println("codeXdminsertbtn");
+
 		System.out.println("dto.getName(): " + dto.getName());
 		System.out.println("dto.getSeq(): " + dto.getSeq());
-		
-	
+
 		service.insert(dto);
-		
+
 		return "redirect:/codeXdmList";
-}
-	
-	
+	}
+
 }
